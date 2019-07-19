aac8aa3e5bfb0771df9fdbdb4c6a16d8b5650998
Giulio Vian <giuliovdev@hotmail.com>
2019-07-19 22:21:03 +0100

    more cleaning

--- src/hosts/terraform/scripts/ssh.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Install OpenSSH.Server"
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Write-Host "Start OpenSSH.Server"
-Start-Service sshd
-Set-Service -Name sshd -StartupType 'Automatic'
