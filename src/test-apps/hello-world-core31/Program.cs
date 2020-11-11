154f81c695f33e78c8e643c005032abbcdb4802f
Giulio Vian <giuliovdev@hotmail.com>
2020-11-11 19:25:13 +0100

    added test apps

---
+++ src/test-apps/hello-world-core31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_core31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
